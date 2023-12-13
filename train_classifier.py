8c23f2d7ce9010d2a4c669105e57e2477c07f33d
Manish Sharma <86907722+Manish-XD@users.noreply.github.com>
2023-12-13 17:09:31 +0530

    Update train_classifier.py

--- train_classifier.py
+++ train_classifier.py
@@ -1,28 +1,31 @@
 import pickle
-
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import numpy as np
 
-
+# Load data from pickle file
 data_dict = pickle.load(open('./data.pickle', 'rb'))
 
-data = np.asarray(data_dict['data'])
+# Normalize shapes of elements in 'data'
+max_length = max(len(entry) for entry in data_dict['data'])
+data = [entry + [0] * (max_length - len(entry)) for entry in data_dict['data']]
+
+# Convert 'labels' to a NumPy array
 labels = np.asarray(data_dict['labels'])
 
+# Continue with the rest of your code
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
 model = RandomForestClassifier()
-
 model.fit(x_train, y_train)
-
 y_predict = model.predict(x_test)
-
 score = accuracy_score(y_predict, y_test)
 
-print('{}% of samples were classified correctly !'.format(score * 100))
+print('{}% of samples were classified correctly!'.format(score * 100))
 
-f = open('model.p', 'wb')
-pickle.dump({'model': model}, f)
-f.close()
+# Save the model to a file
+with open('model.p', 'wb') as f:
+    pickle.dump({'model': model}, f)
+
+
